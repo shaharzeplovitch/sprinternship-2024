524595f10aa85d6fd4f0f351a543841c65afec80
Erica Li <ericali@Ericas-MacBook-Air-2.local>
2024-01-08 13:25:46 -0500

    filterbox creation

---
+++ src/app/filterbox/filterbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filterbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './filterbox.component.html',
+  styleUrl: './filterbox.component.css'
+})
+export class FilterboxComponent {
+
+}
