1e7cf2e79066393e7efba7d885a280ea8d7302b7
Shahar Zeplovitch <szeplovi@szeplovi-mn1.linkedin.biz>
2024-01-11 12:09:18 -0500

    setup company detail page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CompanyDetailComponent } from './company-detail/company-detail.component';
 
 const routeConfig: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home page'
+  },
+  {
+    path: 'company-detail',
+    component: CompanyDetailComponent,
+    title: 'Detail Page'
   }
 ];
 
