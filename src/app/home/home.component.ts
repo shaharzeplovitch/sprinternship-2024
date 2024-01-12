7cabc57580cee5f39682b8a392f3ca1611ad720e
Aleena Tim <aleenatim@Aleenas-MacBook-Air.local>
2024-01-12 13:21:37 -0500

    added suggested companies

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CompanyComponent } from '../company/company.component';
 import { DataService } from '../services/data.service';
 import { Company } from '../interfaces/company';
+import { Experience } from '../interfaces/experience';
 import { FilterboxComponent } from '../filterbox/filterbox.component';
 import { FormsModule } from '@angular/forms';
 import { LeftSidebarMenuComponent } from '../left-sidebar-menu/left-sidebar-menu.component';
+import { SuggestedCompaniesComponent } from '../suggested-companies/suggested-companies.component';
+import { ExperienceComponent } from '../experience/experience.component';
 
 @Component({
   selector: 'app-home',
@@ -15,7 +18,8 @@ import { LeftSidebarMenuComponent } from '../left-sidebar-menu/left-sidebar-menu
     CompanyComponent,
     FormsModule,
     LeftSidebarMenuComponent,
-    FilterboxComponent
+    FilterboxComponent,
+    SuggestedCompaniesComponent
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
@@ -23,9 +27,17 @@ import { LeftSidebarMenuComponent } from '../left-sidebar-menu/left-sidebar-menu
 export class HomeComponent {
   dataService: DataService = inject(DataService);
   companiesList: Company[] = [];
+  // experienceList: Experience[] = [];
+  //  suggestedCompaniesList: SuggestedCompanies[] = [];
   constructor() {
     this.dataService.getAllCompanies().then((companiesList: Company[]) => {
       this.companiesList = companiesList;
     });
+    // this.dataService.getAllExperiences().then((experienceList: Experience[]) => {
+    //   this.experienceList = experienceList;
+    // });
+    // this.dataService.getAllSuggestedCompanies().then((suggestedCompaniesList: SuggestedCompanies[]) => {
+    //   this.suggestedCompaniesList = suggestedCompaniesList;
+    // });
   }
 }
