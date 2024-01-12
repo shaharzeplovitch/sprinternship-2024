f902dc08e48f0a5f8f3a18dc1de02da94a604ddb
Shahar Zeplovitch <szeplovi@szeplovi-mn1.linkedin.biz>
2024-01-12 17:32:04 -0500

    add detail page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ExperiencesOverlayComponent } from './experiences-overlay/experiences-overlay.component';
+import { CompanyDetailComponent } from './company-detail/company-detail.component';
 
 const routeConfig: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home page'
+  },
+  {
+    path: 'company-detail',
+    component: CompanyDetailComponent,
+    title: 'Detail Page'
   },
   {
     path: 'experiences',
