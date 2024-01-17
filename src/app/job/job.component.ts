9fbe0e98c5719ff31cd1193f9f87fcc0b279a4ee
akaricakes <happypooh0928@gmail.com>
2024-01-17 17:18:56 -0500

    saving changes again

--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -1,18 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Job } from '../interfaces/job';
+import { CommonModule } from '@angular/common';
+import { ExperiencesOverlayComponent } from '../experiences-overlay/experiences-overlay.component';
 
 @Component({
   selector: 'app-job',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ExperiencesOverlayComponent],
   templateUrl: './job.component.html',
   styleUrl: './job.component.css'
 })
 export class JobComponent {
   @Input() job!: Job;
+  showOverlay = false;
+  toggleOverlay(){
+    this.showOverlay = true;
+  }
 }
 
-const btn = document.createElement('button');
-const button = document.createElement('button');
-button.textContent = 'Click me';
-document.body.appendChild(button);
