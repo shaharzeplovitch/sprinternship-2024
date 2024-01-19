1a53cd6ed2240cefb3f1576aff17e004d4ab34d4
Shahar Zeplovitch <szeplovi@szeplovi-mn1.linkedin.biz>
2024-01-19 18:07:04 -0500

    saved counter

--- src/app/left-sidebar-menu/left-sidebar-menu.component.ts
+++ src/app/left-sidebar-menu/left-sidebar-menu.component.ts
@@ -1,13 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-left-sidebar-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './left-sidebar-menu.component.html',
-  styleUrl: './left-sidebar-menu.component.css'
+  styleUrls: ['./left-sidebar-menu.component.css']
 })
 export class LeftSidebarMenuComponent {
-  dataService: DataService = inject(DataService);
+  savedCounter: number = 0;
+  dataService: DataService = inject(DataService); // Assuming inject is used for dependency injection
+
+  constructor() {
+    this.updateSavedCounter();
+  }
+
+  updateSavedCounter() {
+    this.savedCounter = this.dataService.savedCounter;
+  }
 }
